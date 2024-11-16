c05e037dc229664f16f63c1ab0966e590f0251b1
woookdev <hjw0911@gmail.com>
2024-11-16 19:05:57 +0900

    feat: add base project

---
+++ LetSwift2024Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetSwift2024Light
+//
+//  Created by Jaewook Hwang on 11/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
