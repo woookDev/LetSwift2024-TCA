d58ea5ac30e4bf655f9176bd0851f78effee5673
woookdev <hjw0911@gmail.com>
2024-11-19 01:02:58 +0900

    feat: 세션 상세 화면 > SessionDetailReducer 구현

---
+++ LetSwift2024Light/Features/SessionDetail/SessionDetailReducer.swift
@@ -0,0 +1,33 @@
+//
+//  SessionDetailReducer.swift
+//  LetSwift2024Light
+//
+//  Created by Jaewook Hwang on 11/16/24.
+//
+
+import ComposableArchitecture
+
+// MARK: - SessionDetailReducer
+
+@Reducer
+struct SessionDetailReducer {
+    // MARK: - State
+
+    @ObservableState
+    struct State: Equatable {
+        var sessionDetailInfoViewModel: SessionDetailInfoViewModel
+        var speakerInfoViewModel: SpeakerInfoViewModel
+    }
+
+    // MARK: - Action
+
+    enum Action {
+        case didTapCompleteButton
+    }
+
+    // MARK: - Reducer
+
+    var body: some Reducer<State, Action> {
+        EmptyReducer()
+    }
+}
