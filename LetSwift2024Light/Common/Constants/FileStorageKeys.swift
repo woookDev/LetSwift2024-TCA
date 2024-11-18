34d5b79ba77b8c6fbb68e4f82a00d44813b35611
woookdev <hjw0911@gmail.com>
2024-11-19 00:51:03 +0900

    feat: 로컬 저장을 위한 FileStorageKeys 추가

---
+++ LetSwift2024Light/Common/Constants/FileStorageKeys.swift
@@ -0,0 +1,12 @@
+//
+//  FileStorageKeys.swift
+//  LetSwift2024Light
+//
+//  Created by Jaewook Hwang on 11/16/24.
+//
+
+import Foundation
+
+enum FileStorageKeys: String {
+    case completedSessions
+}
